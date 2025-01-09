92cc18e696dc0b4c3da664f15f26abebee7540c7
Tushar Rawat <161054503+TusharRawat01@users.noreply.github.com>
2025-01-09 15:57:57 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello{  
 public static void main(String[] args){  
-System.out.println("This is java app \n using for jenkins");  
+System.out.println("This is java app \n using for jenkins");
+  System.out.println("Secons change"); 
 }  
 }  
